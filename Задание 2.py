a39c55ec4448b63c8d8256daaff92c8756c0c2ca
Ressset <62643074+Ressset@users.noreply.github.com>
2021-10-03 19:38:18 +0500

    Update Задание 2.py

--- Задание 2.py
+++ Задание 2.py
@@ -1,14 +1,20 @@
 print(" ")
-print("Задание #2")
-el_count = int(input(" - Введите количество элементов списка: "))
-my_list = []
-i = 0
-el = 0
-while i < el_count:
-    my_list.append(input("Введите следующее значение списка: "))
-    i += 1
+print("Задание #3")
+seasons_list = ['RUS:  Зима', 'RUS:  Весна', 'RUS:  Лето', 'RUS:  Осень']
+seasons_dict = {1 : 'ENG:  Winter', 2 : 'ENG:  Spring', 3 : 'ENG:  Summer', 4 : 'ENG:  Autumn'}
+month = int(input(" - Введите номер месяца: "))
+if month ==1 or month == 12 or month == 2:
+        print(seasons_dict.get(1))
+        print(seasons_list[0])
+elif month == 3 or month == 4 or month ==5:
+    print(seasons_dict.get(2))
+    print(seasons_list[1])
+elif month == 6 or month == 7 or month == 8:
+    print(seasons_dict.get(3))
+    print(seasons_list[2])
 
-for elem in range(int(len(my_list)/2)):
-        my_list[el], my_list[el + 1] = my_list [el + 1], my_list[el]
-        el += 2
-print(my_list)
+elif month == 9 or month == 10 or month == 11:
+    print(seasons_dict.get(4))
+    print(seasons_list[3])
+else:
+        print(" - Не корректная дата.Такого месяца не существует.")
