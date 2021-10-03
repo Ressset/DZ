996295305969d44459381b664e4100fabfa31af7
Ressset <62643074+Ressset@users.noreply.github.com>
2021-10-03 19:22:23 +0500

    Add files via upload

---
+++ Задание 2.py
@@ -0,0 +1,9 @@
+print(" ")
+print("Задание #2")
+el_count = int(input(" - Введите количество элементов списка: "))
+my_list = []
+i = 0
+el = 0
+while i < el_count:
+    my_list.append(input("Введите следующее значение списка: "))
+    i += 1
